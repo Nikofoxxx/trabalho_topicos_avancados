3197657c9c464edaa79c20184fd422156a73a5ed
nicolas <nicolashenrique2@hotmail.com>
2015-12-01 21:08:17 -0200

    Criação da interação entre as activities

---
+++ app/src/main/java/univas/edu/br/bookssearch/ListUnlikedBooksActivity.java
@@ -0,0 +1,47 @@
+package univas.edu.br.bookssearch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by nicolas on 01/12/15.
+ */
+public class ListUnlikedBooksActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_unliked_books);
+        configureButtons();
+    }
+
+    private void configureButtons() {
+        Button goHome = (Button) super.findViewById(R.id.unliked_to_homeBtn);
+        goHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/univas/edu/br/bookssearch/MainActivity.java
+++ app/src/main/java/univas/edu/br/bookssearch/MainActivity.java
@@ -1,9 +1,12 @@
 package univas.edu.br.bookssearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureButtons();
+    }
+
+    private void configureButtons() {
+        Button listLikedBooksBtn = (Button) super.findViewById(R.id.listLikedBooksBtn);
+        listLikedBooksBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListLikedBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button listUnlikedBooksBtn = (Button) super.findViewById(R.id.listUnlikedBooksBtn);
+        listUnlikedBooksBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListUnlikedBooksActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
